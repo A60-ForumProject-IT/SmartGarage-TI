4d557123cf855e72a837bec8012bfb974724733c
IHKaragyozov19 <ilian_k_19@abv.bg>
2024-08-21 16:08:45 +0300

    Initial commit

---
+++ src/main/java/com/telerikacademy/web/smartgarageti/SmartGarageTiApplication.java
@@ -0,0 +1,13 @@
+package com.telerikacademy.web.smartgarageti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGarageTiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartGarageTiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/telerikacademy/web/smartgarageti/SmartGarageTiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telerikacademy.web.smartgarageti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGarageTiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
