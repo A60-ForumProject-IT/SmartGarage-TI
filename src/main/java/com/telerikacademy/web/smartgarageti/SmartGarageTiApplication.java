d681081c9d59737717e6addb16a271724eafffa9
Todor Raynov <t.raynov@abv.bg>
2024-08-21 16:13:16 +0300

    Test clear

--- src/main/java/com/telerikacademy/web/smartgarageti/SmartGarageTiApplication.java
+++ src/main/java/com/telerikacademy/web/smartgarageti/SmartGarageTiApplication.java
@@ -8,7 +8,6 @@ public class SmartGarageTiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SmartGarageTiApplication.class, args);
-        System.out.println("Toshko");
     }
 
 }
