1c32143c0dcdfd0caa114c3115ace2713f6dfde7
IHKaragyozov19 <ilian_k_19@abv.bg>
2024-08-22 13:22:31 +0300

    Create packages for the controllers

---
+++ src/main/java/com/telerikacademy/web/smartgarageti/controllers/mvc/UserMvcController.java
@@ -0,0 +1,4 @@
+package com.telerikacademy.web.smartgarageti.controllers.mvc;
+
+public class UserMvcController {
+}
--- src/main/java/com/telerikacademy/web/smartgarageti/controllers/rest/UserRestController.java
+++ src/main/java/com/telerikacademy/web/smartgarageti/controllers/rest/UserRestController.java
@@ -1,4 +1,9 @@
 package com.telerikacademy.web.smartgarageti.controllers.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserRestController {
 }
