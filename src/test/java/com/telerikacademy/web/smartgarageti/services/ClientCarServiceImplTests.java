48aeffa9c65c771e2c75d66a22ed3297c0fb47e1
Todor Raynov <t.raynov@abv.bg>
2024-08-27 15:51:13 +0300

    ClientCarServiceImplTests implemented!

--- src/test/java/com/telerikacademy/web/smartgarageti/services/ClientCarServiceImplTests.java
+++ src/test/java/com/telerikacademy/web/smartgarageti/services/ClientCarServiceImplTests.java
@@ -1,18 +1,31 @@
 package com.telerikacademy.web.smartgarageti.services;
 
 import com.telerikacademy.web.smartgarageti.exceptions.DuplicateEntityException;
-import com.telerikacademy.web.smartgarageti.helpers.TestHelpers;
+import com.telerikacademy.web.smartgarageti.exceptions.EntityNotFoundException;
+import com.telerikacademy.web.smartgarageti.exceptions.NoResultsFoundException;
+import com.telerikacademy.web.smartgarageti.exceptions.UnauthorizedOperationException;
 import com.telerikacademy.web.smartgarageti.models.ClientCar;
+import com.telerikacademy.web.smartgarageti.models.User;
 import com.telerikacademy.web.smartgarageti.repositories.contracts.ClientCarRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.data.domain.Sort;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+
+import static com.telerikacademy.web.smartgarageti.helpers.TestHelpers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ClientCarServiceImplTests {
@@ -23,13 +36,174 @@ public class ClientCarServiceImplTests {
     @InjectMocks
     private ClientCarServiceImpl carService;
 
+
     @Test
     public void createClientCar_ShouldThrow_IfVinExists() {
-        ClientCar carToBeCreated = TestHelpers.createMockClientCar();
+        ClientCar carToBeCreated = createMockClientCar();
 
-        Mockito.when(clientCarRepository.findByVin(carToBeCreated.getVin()))
+        when(clientCarRepository.findByVin(carToBeCreated.getVin()))
                 .thenReturn(Optional.of(carToBeCreated));
 
         Assertions.assertThrows(DuplicateEntityException.class, () -> carService.createClientCar(carToBeCreated));
+    }
+
+    @Test
+    public void createClientCar_ShouldThrow_IfLicensePlateExists() {
+        ClientCar carToBeCreated = createMockClientCar();
+
+        when(clientCarRepository.findByLicensePlate(carToBeCreated.getLicensePlate()))
+                .thenReturn(Optional.of(carToBeCreated));
+
+        Assertions.assertThrows(DuplicateEntityException.class, () -> carService.createClientCar(carToBeCreated));
+    }
+
+    @Test
+    public void createClientCar_ShouldCreateClientCar_WhenEverythingIsValid() {
+        ClientCar mockClientCar = createMockClientCar();
+
+        when(clientCarRepository.findByVin(anyString())).thenReturn(Optional.empty());
+        when(clientCarRepository.findByLicensePlate(anyString())).thenReturn(Optional.empty());
+        when(clientCarRepository.save(mockClientCar)).thenReturn(mockClientCar);
+
+        ClientCar result = carService.createClientCar(mockClientCar);
+
+        assertEquals(mockClientCar, result);
+        verify(clientCarRepository).save(mockClientCar);
+    }
+
+    @Test
+    public void updateShouldThrow_IfUserIsNotEmployee() {
+        ClientCar clientCar = createMockClientCar();
+        User mockUser = createMockUser();
+
+        assertThrows(UnauthorizedOperationException.class,
+                () -> carService.updateClientCar(clientCar, mockUser));
+    }
+
+    @Test
+    public void updateClientCar_ShouldUpdateClientCar_WhenUserIsValid() {
+        ClientCar clientCar = createMockClientCar();
+        User mockUser = createMockUserEmployee();
+        clientCar.setOwner(mockUser);
+
+        carService.updateClientCar(clientCar, mockUser);
+
+        verify(clientCarRepository).save(clientCar);
+    }
+
+    @Test
+    public void getClientCars_ShouldThrow_IfNoResultsFound() {
+        ClientCar clientCar = createMockClientCar();
+        User mockUser = createMockUser();
+        User carOwner = createMockUser();
+        clientCar.setOwner(carOwner);
+
+        assertThrows(NoResultsFoundException.class,
+                () -> carService.getClientCarsByClientId(carOwner.getId(), mockUser, carOwner));
+    }
+
+    @Test
+    @MockitoSettings(strictness = Strictness.LENIENT)
+    public void getClientCars_ShouldThrow_IfCarsAreFound() {
+        int clientId = 1;
+        User mockLoggedInUser = createMockUserEmployee();
+        User mockCarOwner = createMockUser();
+        List<ClientCar> mockClientCars = List.of(createMockClientCar());
+
+        when(clientCarRepository.findAllByOwnerId(clientId)).thenReturn(mockClientCars);
+
+        List<ClientCar> result = carService.getClientCarsByClientId(clientId, mockLoggedInUser, mockCarOwner);
+
+        assertEquals(mockClientCars, result);
+        verify(clientCarRepository).findAllByOwnerId(clientId);
+    }
+
+    @Test
+    void filterAndSortClientCarsByOwner_ShouldReturnSortedClientCars_WhenCarsExist() {
+        User mockLoggedInUser = createMockUserEmployee();
+        String searchTerm = "search";
+        String sortBy = "username";
+        String sortDirection = "asc";
+        Sort sort = Sort.by(Sort.Direction.ASC, sortBy);
+
+        List<ClientCar> mockClientCars = List.of(createMockClientCar());
+
+        when(clientCarRepository.findAllByOwnerUsernameContainingIgnoreCaseOrOwnerFirstNameContainingIgnoreCase(
+                searchTerm, searchTerm, sort)).thenReturn(mockClientCars);
+
+        List<ClientCar> result = carService.filterAndSortClientCarsByOwner(mockLoggedInUser, searchTerm, sortBy, sortDirection);
+
+        assertEquals(mockClientCars, result);
+        verify(clientCarRepository).findAllByOwnerUsernameContainingIgnoreCaseOrOwnerFirstNameContainingIgnoreCase(
+                searchTerm, searchTerm, sort);
+    }
+
+    @Test
+    void filterAndSortClientCarsByOwner_ShouldThrowNoResultsFoundException_WhenNoCarsFound() {
+        User mockLoggedInUser = createMockUserEmployee();
+        String searchTerm = "search";
+        String sortBy = "username";
+        String sortDirection = "asc";
+        Sort sort = Sort.by(Sort.Direction.ASC, sortBy);
+
+        when(clientCarRepository.findAllByOwnerUsernameContainingIgnoreCaseOrOwnerFirstNameContainingIgnoreCase(
+                searchTerm, searchTerm, sort)).thenReturn(Collections.emptyList());
+
+        assertThrows(NoResultsFoundException.class, () ->
+                carService.filterAndSortClientCarsByOwner(mockLoggedInUser, searchTerm, sortBy, sortDirection));
+    }
+
+    @Test
+    void filterAndSortClientCarsByOwner_ShouldUseDescendingSort_WhenSortDirectionIsDesc() {
+        User mockLoggedInUser = createMockUserEmployee();
+        String searchTerm = "search";
+        String sortBy = "username";
+        String sortDirection = "desc";
+        Sort sort = Sort.by(Sort.Direction.DESC, sortBy);
+
+        List<ClientCar> mockClientCars = List.of(createMockClientCar());
+
+        when(clientCarRepository.findAllByOwnerUsernameContainingIgnoreCaseOrOwnerFirstNameContainingIgnoreCase(
+                searchTerm, searchTerm, sort)).thenReturn(mockClientCars);
+
+        List<ClientCar> result = carService.filterAndSortClientCarsByOwner(mockLoggedInUser, searchTerm, sortBy, sortDirection);
+
+        assertEquals(mockClientCars, result);
+        verify(clientCarRepository).findAllByOwnerUsernameContainingIgnoreCaseOrOwnerFirstNameContainingIgnoreCase(
+                searchTerm, searchTerm, sort);
+    }
+
+    @Test
+    void findByLicensePlate_ShouldThrow_WhenLicensePlateDoesNotExists() {
+        ClientCar mockClientCar = createMockClientCar();
+
+        assertThrows(EntityNotFoundException.class, () -> carService.findByLicensePlate(mockClientCar.getLicensePlate()));
+    }
+
+    @Test
+    void findByVin_ShouldThrow_WhenVinPlateDoesNotExists() {
+        ClientCar mockClientCar = createMockClientCar();
+
+        assertThrows(EntityNotFoundException.class, () -> carService.findByVin(mockClientCar.getVin()));
+    }
+
+    @Test
+    void getClientCarById_ShouldThrowEntityNotFoundException_WhenCarDoesNotExist() {
+        int carId = 1;
+        when(clientCarRepository.findById(carId)).thenReturn(Optional.empty());
+
+        assertThrows(EntityNotFoundException.class, () -> carService.getClientCarById(carId));
+        verify(clientCarRepository).findById(carId);
+    }
+
+    @Test
+    void getAllClientCars_Should_Pass() {
+        List<ClientCar> clientCars = List.of(createMockClientCar());
+
+        when(clientCarRepository.findAll()).thenReturn(clientCars);
+
+        carService.getAllClientCars();
+
+        verify(clientCarRepository).findAll();
     }
 }
