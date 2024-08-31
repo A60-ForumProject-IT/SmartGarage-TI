4026d3d50ee415ef25e73a07d4912093b09fd723
Todor Raynov <t.raynov@abv.bg>
2024-08-31 12:59:32 +0300

    ClientCarsMvcController, ClientCars.html created. Styles.css updated for lining up the elements in the HTML.

---
+++ src/main/java/com/telerikacademy/web/smartgarageti/controllers/mvc/ClientCarsMvcController.java
@@ -0,0 +1,25 @@
+package com.telerikacademy.web.smartgarageti.controllers.mvc;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/ti/client-cars")
+public class ClientCarsMvcController {
+
+    @ModelAttribute("isAuthenticated")
+    public boolean populateIsAuthenticated(HttpSession session) {
+        return session.getAttribute("currentUser") != null;
+    }
+
+    @GetMapping
+    public String showClientCars(Model model) {
+        return "ClientCars";
+    }
+}
