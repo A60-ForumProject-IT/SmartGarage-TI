7cbd84c8ff94412a39573e385f9c6c6c44047953
IHKaragyozov19 <ilian_k_19@abv.bg>
2024-09-06 15:09:53 +0300

    Mechanic added

--- src/main/java/com/telerikacademy/web/smartgarageti/helpers/PermissionHelper.java
+++ src/main/java/com/telerikacademy/web/smartgarageti/helpers/PermissionHelper.java
@@ -5,6 +5,7 @@ import com.telerikacademy.web.smartgarageti.models.User;
 
 public class PermissionHelper {
     public static final String ROLE_EMPLOYEE = "Employee";
+    public static final String ROLE_MECHANIC = "Mechanic";
 
     public static void isEmployee(User authenticatedUser, String message) {
         boolean isEmployee = false;
@@ -32,6 +33,12 @@ public class PermissionHelper {
 
     public static void isEmployeeOrSameUser(User user, User userToDelete, String message) {
         if ( user.getId() != userToDelete.getId() && !user.getRole().getName().equals(ROLE_EMPLOYEE)) {
+            throw new UnauthorizedOperationException(message);
+        }
+    }
+
+    public static void isMechanic(User authenticatedUser, String message) {
+        if (!authenticatedUser.getRole().getName().equals(ROLE_MECHANIC)) {
             throw new UnauthorizedOperationException(message);
         }
     }
